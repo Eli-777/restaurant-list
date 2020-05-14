c6196989b3adc39443278b7a79c53eb819bcbf65
mrsandwitch <mrsandwitch@gmail.com>
2020-05-14 20:05:36 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-// const restaurantList = require('./restaurant.json')
 const Restaurant = require('./models/restaurant.js')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -20,6 +20,8 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
@@ -27,6 +29,26 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//新增餐廳頁面
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+//取得表單資訊並新增
+app.post('/restaurants',(req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return  Restaurant.create({ name, name_en , category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 // app.get('/restaurants/:restaurant_id', (req, res) => {
 //   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
 //   res.render('show', { restaurant: restaurant })
