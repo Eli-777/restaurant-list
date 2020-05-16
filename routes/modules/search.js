4690c71d40020d5a6b73c38af903d502b5823a2f
Eli <jo45840@hotmail.com>
2020-05-16 17:35:56 +0800

    feat: add sort function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,9 +4,12 @@ const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
   const keyword = req.query.keyword
+  const order = req.query.order
+  const target = req.query.target
   Restaurant.find({ $or: [{ name: new RegExp(keyword, 'i') }, { category: new RegExp(keyword, 'i') }] })
     .lean()
-    .then(restaurants => res.render('index', { restaurants, keyword }))
+    .sort({ [target]: order })
+    .then(restaurants => res.render('index', { restaurants , keyword}))
     .catch(error => console.log(error))
 })
 
